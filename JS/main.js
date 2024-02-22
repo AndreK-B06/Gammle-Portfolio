4e77c56f347aeb7bb9c698263be5bd2106ce7fa2
André <137167063+AndreK-B06@users.noreply.github.com>
2024-02-22 11:21:19 +0100

    farge forandringer

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,2 @@
-// const github = document.querySelector("#Github");
-
-// github.addEventListener("click", function () {
-//   window.open("https://github.com/AndreK-B06?tab=repositories", "_blank");
-// });
-// github();
+let vid = document.getElementById("source");
+vid.playrate = 1;
