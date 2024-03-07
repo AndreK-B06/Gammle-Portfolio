f634ef30f222018e7f8bf5a7b3dc796ef0fa4475
André <137167063+AndreK-B06@users.noreply.github.com>
2024-03-07 07:42:38 +0100

    lagt til js og lakt til id

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,24 @@
-document.querySelector("video").playbackRate = 0.4;
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("video").playbackRate = 0.4;
+  const omMegBtn = document.querySelector("#om-meg-btn");
+  const kontaktMegBtn = document.querySelector("#kontakt-meg-btn");
+  //
+  //
+  //
+  omMegBtn.addEventListener("click", function () {
+    console.log("clickt");
+    omMegBtn.scroll({
+      top: omMegBtn.clientHeight / 1,
+      behavior: "smooth",
+    });
+  });
+  //
+  kontaktMegBtn.addEventListener("click", function () {
+    console.log("clickt");
+    footer.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "nearest",
+    });
+  });
+});
